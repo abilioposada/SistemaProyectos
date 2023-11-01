a915282153bb0bf03ba2e7096f37b82f66dbe21a
Abilio Posada <abilioposada@gmail.com>
2023-10-31 20:48:20 -0600

    Crear modelo con andamiaje

---
+++ tests/Feature/Models/ProyectoTest.php
@@ -0,0 +1,18 @@
+<?php namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProyectoTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get( '/' );
+
+        $response->assertStatus( 200 );
+    }
+}
