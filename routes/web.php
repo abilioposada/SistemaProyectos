f8f934434cd6979cd0fe56984777785a8ba5c520
Abilio Posada <abilioposada@gmail.com>
2023-11-02 03:58:08 -0600

    Actualizar página de bienvenida

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get( '/', function () {
-    return view( 'welcome' );
-} );
+Route::get( '/', fn () => view( "bienvenida" ) );
+
+Route::resource( "proyectos", \App\Http\Controllers\ProyectoController::class );
